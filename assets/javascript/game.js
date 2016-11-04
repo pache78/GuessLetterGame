75648788e346955ce55a5c57091694ca7f9d491e
pache78 <jmpacheco78@gmail.com>
2016-11-03 19:21:51 -0500

    HW_WK3

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+
+// Generate a ramdom letter that will be placed in a variable for later use.//
+var Rletter = Math.floor(Math.random() * Rletter) + 1;
+
+
+//User begins guessing a letter between A-Z and will be displayed in "guessesSoFar". Player will be prompted to start guessing.//
+//prompt("Please pick a  random letter between A and " + Rletter)
+
+//Letter will be verified, if correct one was selected a point is placed in "wins" location. //
+
+
+//Letter guessed wrong the "guessesLeft" will display how many attempts are left. This will be repeated until user goes through number of attempts//
+
+
+//Player runs out of tries, "losses" will be displayed.//
+
+
+//reset game//
+
